fefe815a928632cab61f51a42cbb9ca9ab48e4c0
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2024-09-23 07:26:30 +0530

    Contract Updated

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -37,20 +37,35 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
 contract Raffle is VRFConsumerBaseV2Plus {
     /* Errors */
     error Raffle__SendMoreToEnterRaffle();
-    error Raffle__StillTimeLeftToAnnounceWinners();
+    error Raffle__TransferToWinnerFailed();
+    error Raffle__RaffleNotOpen();
+    error Raffle__StillTimeLeftToAnnounceWinner();
 
+    /** Type Decalrations */
+
+    enum RaffleStatus {
+        OPEN,
+        CALCULATING
+    }
+
+    /** State Variables */
     uint32 private constant MAX_WORDS = 1;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
+
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_inteval;
     bytes32 private immutable i_keyHash;
     uint256 private immutable i_subscriptionID;
     uint32 private immutable i_gasLimit;
+
     uint256 private s_lastTimeStamp;
     address payable[] private s_players;
+    address private s_recentWinner;
+    RaffleStatus private s_raffleStatus;
 
     /**Events */
     event RaffleEntered(address indexed player);
+    event PickedWinner(address indexed winner);
 
     constructor(
         uint256 entranceFee,
@@ -65,13 +80,16 @@ contract Raffle is VRFConsumerBaseV2Plus {
         i_keyHash = keyhash;
         i_gasLimit = gasLimit;
         i_subscriptionID = subcriptionID;
+
         s_lastTimeStamp = block.timestamp;
+        s_raffleStatus = RaffleStatus.OPEN;
     }
 
     function enterRaffle() external payable {
         require(msg.value >= i_entranceFee, Raffle__SendMoreToEnterRaffle());
-        s_players.push(payable(msg.sender));
+        require(s_raffleStatus == RaffleStatus.OPEN, Raffle__RaffleNotOpen());
 
+        s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
     }
 
@@ -81,8 +99,9 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function pickWinner() external {
         require(
             block.timestamp - s_lastTimeStamp > i_inteval,
-            Raffle__StillTimeLeftToAnnounceWinners()
+            Raffle__StillTimeLeftToAnnounceWinner()
         );
+        s_raffleStatus = RaffleStatus.CALCULATING;
 
         VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
             .RandomWordsRequest({
@@ -102,7 +121,18 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] calldata randomWords
-    ) internal override {}
+    ) internal override {
+        uint256 indexOfPlayer = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfPlayer];
+        s_recentWinner = recentWinner;
+        s_raffleStatus = RaffleStatus.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit PickedWinner(s_recentWinner);
+        //Transfer To the winner
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        require(success, Raffle__TransferToWinnerFailed());
+    }
 
     /**
      * Getter Functions
